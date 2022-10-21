6e82875ca675b4270ccc6297f4a2c8ba5d11a9cb
RES0132082 <RES0132082@SEN7401>
2022-10-21 16:46:09 -0300

    PalavraCruzada_v2

--- PalavraCruzada/MainWindow.xaml.cs
+++ PalavraCruzada/MainWindow.xaml.cs
@@ -20,7 +20,16 @@ namespace PalavraCruzada
     /// </summary>
     public partial class MainWindow : Window
     {
-        int PalavrasAcertatas = 0;
+        bool Palavra1 = false;
+        bool Palavra2 = false;
+        bool Palavra3 = false;
+        bool Palavra4 = false;
+        bool Palavra5 = false;
+        bool Palavra6 = false;
+        bool Palavra7 = false;
+        bool Palavra8 = false;
+        bool Palavra9 = false;
+        bool Palavra10 = false;
         public MainWindow()
         {
             InitializeComponent();
@@ -78,57 +87,272 @@ namespace PalavraCruzada
 
         private void VerificarP1(object sender, KeyEventArgs e)
         {
-            if (C1_1.Text.Contains("Q") && C1_2.Text.Contains("U") && C1_3.Text.Contains("E") && C1_4.Text.Contains("I") && C1_5.Text.Contains("J") && C1_6.Text.Contains("O"))
+            if (C1_1.Text.Contains("Q") && C1_2.Text.Contains("U") && C1_3.Text.Contains("E") && C1_4.Text.Contains("I") && C1_5.Text.Contains("J") && C1_6.Text.Contains("O") && Palavra1 == false)
             {
-                MessageBoxResult result = MessageBox.Show("PALAVRA 1 COMPLETA", "Dica:");
+                C1_1.Background = Brushes.LightGreen;
+                C1_2.Background = Brushes.LightGreen;
+                C1_3.Background = Brushes.LightGreen;
+                C1_4.Background = Brushes.LightGreen;
+                C1_5.Background = Brushes.LightGreen;
+                C1_6.Background = Brushes.LightGreen;
+                MessageBoxResult result = MessageBox.Show("PALAVRA 1 COMPLETA", "Parabéns:");
+                Palavra1 = true;
+            }
+
+            if (C1_4.Text.Contains("I") && C2_2.Text.Contains("G") && C2_3.Text.Contains("R") && C2_4.Text.Contains("E") && C2_5.Text.Contains("J") && C2_6.Text.Contains("A") && Palavra2 == false)
+            {
+                C1_4.Background = Brushes.LightGreen;
+                C2_2.Background = Brushes.LightGreen;
+                C2_3.Background = Brushes.LightGreen;
+                C2_4.Background = Brushes.LightGreen;
+                C2_5.Background = Brushes.LightGreen;
+                C2_6.Background = Brushes.LightGreen;
+                MessageBoxResult result = MessageBox.Show("PALAVRA 2 COMPLETA", "Parabéns:");
+                Palavra2 = true;
+            }
+
+            if (C3_1.Text.Contains("J") && C1_6.Text.Contains("O") && C3_2.Text.Contains("R") && C3_3.Text.Contains("N") && C3_4.Text.Contains("A") && C3_5.Text.Contains("L") && Palavra3 == false)
+            {
+                C3_1.Background = Brushes.LightGreen;
+                C1_6.Background = Brushes.LightGreen;
+                C3_2.Background = Brushes.LightGreen;
+                C3_3.Background = Brushes.LightGreen;
+                C3_4.Background = Brushes.LightGreen;
+                C3_5.Background = Brushes.LightGreen;
+                MessageBoxResult result = MessageBox.Show("PALAVRA 3 COMPLETA", "Parabéns:");
+                Palavra3 = true;
             }
         }
 
         private void VerificarP2(object sender, KeyEventArgs e)
         {
-            if (C1_4.Text.Contains("I") && C2_2.Text.Contains("G") && C2_3.Text.Contains("R") && C2_4.Text.Contains("E") && C2_5.Text.Contains("J") && C2_6.Text.Contains("A"))
+            if (C1_4.Text.Contains("I") && C2_2.Text.Contains("G") && C2_3.Text.Contains("R") && C2_4.Text.Contains("E") && C2_5.Text.Contains("J") && C2_6.Text.Contains("A") && Palavra2 == false)
             {
-                MessageBoxResult result = MessageBox.Show("PALAVRA 2 COMPLETA", "Dica:");
+                    C1_4.Background = Brushes.LightGreen;
+                    C2_2.Background = Brushes.LightGreen;
+                    C2_3.Background = Brushes.LightGreen;
+                    C2_4.Background = Brushes.LightGreen;
+                    C2_5.Background = Brushes.LightGreen;
+                    C2_6.Background = Brushes.LightGreen;
+                    MessageBoxResult result = MessageBox.Show("PALAVRA 2 COMPLETA", "Parabéns:");
+                    Palavra2 = true;
             }
         }
 
         private void VerificarP3(object sender, KeyEventArgs e)
         {
-            if (C3_1.Text.Contains("J") && C1_6.Text.Contains("O") && C3_2.Text.Contains("R") && C3_3.Text.Contains("N") && C3_4.Text.Contains("A") && C3_5.Text.Contains("L"))
+            if (C3_1.Text.Contains("J") && C1_6.Text.Contains("O") && C3_2.Text.Contains("R") && C3_3.Text.Contains("N") && C3_4.Text.Contains("A") && C3_5.Text.Contains("L") && Palavra3 == false)
             {
-                MessageBoxResult result = MessageBox.Show("PALAVRA 3 COMPLETA", "Dica:");
+                C3_1.Background = Brushes.LightGreen;
+                C1_6.Background = Brushes.LightGreen;
+                C3_2.Background = Brushes.LightGreen;
+                C3_3.Background = Brushes.LightGreen;
+                C3_4.Background = Brushes.LightGreen;
+                C3_5.Background = Brushes.LightGreen;
+                MessageBoxResult result = MessageBox.Show("PALAVRA 3 COMPLETA", "Parabéns:");
+                Palavra3 = true;
             }
+
+            if (C3_2.Text.Contains("R") && C7_2.Text.Contains("O") && C7_3.Text.Contains("M") && C4_2.Text.Contains("A") && Palavra7 == false)
+            {
+                C3_2.Background = Brushes.LightGreen;
+                C7_2.Background = Brushes.LightGreen;
+                C7_3.Background = Brushes.LightGreen;
+                C4_2.Background = Brushes.LightGreen;
+                MessageBoxResult result = MessageBox.Show("PALAVRA 7 COMPLETA", "Parabéns:");
+                Palavra7 = true;
+            }
+
         }
 
         private void VerificarP4(object sender, KeyEventArgs e)
         {
-            if (C4_1.Text.Contains("C") && C4_2.Text.Contains("A") && C4_3.Text.Contains("R") && C4_4.Text.Contains("T") && C4_5.Text.Contains("A"))
+            if (C4_1.Text.Contains("C") && C4_2.Text.Contains("A") && C4_3.Text.Contains("R") && C4_4.Text.Contains("T") && C4_5.Text.Contains("A") && Palavra4 == false)
             {
-                MessageBoxResult result = MessageBox.Show("PALAVRA 4 COMPLETA", "Dica:");
+                C4_1.Background = Brushes.LightGreen;
+                C4_2.Background = Brushes.LightGreen;
+                C4_3.Background = Brushes.LightGreen;
+                C4_4.Background = Brushes.LightGreen;
+                C4_5.Background = Brushes.LightGreen;
+                MessageBoxResult result = MessageBox.Show("PALAVRA 4 COMPLETA", "Parabéns:");
+                Palavra4 = true;
+            }
+
+            if (C3_2.Text.Contains("R") && C7_2.Text.Contains("O") && C7_3.Text.Contains("M") && C4_2.Text.Contains("A") && Palavra7 == false)
+            {
+                C3_2.Background = Brushes.LightGreen;
+                C7_2.Background = Brushes.LightGreen;
+                C7_3.Background = Brushes.LightGreen;
+                C4_2.Background = Brushes.LightGreen;
+                MessageBoxResult result = MessageBox.Show("PALAVRA 7 COMPLETA", "Parabéns:");
+                Palavra7 = true;
+            }
+
+            if (C4_1.Text.Contains("C") && C9_2.Text.Contains("U") && C9_3.Text.Contains("B") && C9_4.Text.Contains("O") && Palavra9 == false)
+            {
+                C4_1.Background = Brushes.LightGreen;
+                C9_2.Background = Brushes.LightGreen;
+                C9_3.Background = Brushes.LightGreen;
+                C9_4.Background = Brushes.LightGreen;
+                MessageBoxResult result = MessageBox.Show("PALAVRA 9 COMPLETA", "Parabéns:");
+                Palavra9 = true;
+            }
+
+            if (C8_1.Text.Contains("C") && C4_5.Text.Contains("A") && C8_3.Text.Contains("M") && C5_2.Text.Contains("I") && C8_5.Text.Contains("S") && C8_6.Text.Contains("E") && C8_7.Text.Contains("T") && C6_2.Text.Contains("A") && Palavra8 == false)
+            {
+                C8_1.Background = Brushes.LightGreen;
+                C4_5.Background = Brushes.LightGreen;
+                C8_3.Background = Brushes.LightGreen;
+                C5_2.Background = Brushes.LightGreen;
+                C8_5.Background = Brushes.LightGreen;
+                C8_6.Background = Brushes.LightGreen;
+                C8_7.Background = Brushes.LightGreen;
+                C6_2.Background = Brushes.LightGreen;
+                MessageBoxResult result = MessageBox.Show("PALAVRA 8 COMPLETA", "Parabéns:");
+                Palavra8 = true;
             }
         }
 
         private void VerificarP5(object sender, KeyEventArgs e)
         {
-            if (C5_1.Text.Contains("P") && C5_2.Text.Contains("I") && C5_3.Text.Contains("R") && C5_4.Text.Contains("U") && C5_5.Text.Contains("L") && C5_6.Text.Contains("I") && C5_7.Text.Contains("L") && C5_8.Text.Contains("O"))
+            if (C5_1.Text.Contains("P") && C5_2.Text.Contains("I") && C5_3.Text.Contains("R") && C5_4.Text.Contains("U") && C5_5.Text.Contains("L") && C5_6.Text.Contains("I") && C5_7.Text.Contains("T") && C5_8.Text.Contains("O") && Palavra5 == false)
             {
-                MessageBoxResult result = MessageBox.Show("PALAVRA 5 COMPLETA", "Dica:");
+                C5_1.Background = Brushes.LightGreen;
+                C5_2.Background = Brushes.LightGreen;
+                C5_3.Background = Brushes.LightGreen;
+                C5_4.Background = Brushes.LightGreen;
+                C5_5.Background = Brushes.LightGreen;
+                C5_6.Background = Brushes.LightGreen;
+                C5_7.Background = Brushes.LightGreen;
+                C5_8.Background = Brushes.LightGreen;
+                MessageBoxResult result = MessageBox.Show("PALAVRA 5 COMPLETA", "Parabéns:");
+                Palavra5 = true;
+            }
+
+            if (C8_1.Text.Contains("C") && C4_5.Text.Contains("A") && C8_3.Text.Contains("M") && C5_2.Text.Contains("I") && C8_5.Text.Contains("S") && C8_6.Text.Contains("E") && C8_7.Text.Contains("T") && C6_2.Text.Contains("A") && Palavra8 == false)
+            {
+                C8_1.Background = Brushes.LightGreen;
+                C4_5.Background = Brushes.LightGreen;
+                C8_3.Background = Brushes.LightGreen;
+                C5_2.Background = Brushes.LightGreen;
+                C8_5.Background = Brushes.LightGreen;
+                C8_6.Background = Brushes.LightGreen;
+                C8_7.Background = Brushes.LightGreen;
+                C6_2.Background = Brushes.LightGreen;
+                MessageBoxResult result = MessageBox.Show("PALAVRA 8 COMPLETA", "Parabéns:");
+                Palavra8 = true;
+            }
+
+            if (C10_1.Text.Contains("F") && C5_8.Text.Contains("O") && C10_3.Text.Contains("N") && C10_4.Text.Contains("E") && Palavra10 == false)
+            {
+                C10_1.Background = Brushes.LightGreen;
+                C5_8.Background = Brushes.LightGreen;
+                C10_3.Background = Brushes.LightGreen;
+                C10_4.Background = Brushes.LightGreen;
+                MessageBoxResult result = MessageBox.Show("PALAVRA 9 COMPLETA", "Parabéns:");
+                Palavra10 = true;
             }
         }
 
         private void VerificarP6(object sender, KeyEventArgs e)
         {
-            if (C6_1.Text.Contains("X") && C6_2.Text.Contains("A") && C6_3.Text.Contains("R") && C6_4.Text.Contains("O") && C6_5.Text.Contains("P") && C6_6.Text.Contains("E"))
+            if (C6_1.Text.Contains("X") && C6_2.Text.Contains("A") && C6_3.Text.Contains("R") && C6_4.Text.Contains("O") && C6_5.Text.Contains("P") && C6_6.Text.Contains("E") && Palavra6 == false)
             {
-                MessageBoxResult result = MessageBox.Show("PALAVRA 6 COMPLETA", "Dica:");
+                C6_1.Background = Brushes.LightGreen;
+                C6_2.Background = Brushes.LightGreen;
+                C6_3.Background = Brushes.LightGreen;
+                C6_4.Background = Brushes.LightGreen;
+                C6_5.Background = Brushes.LightGreen;
+                C6_6.Background = Brushes.LightGreen;
+                MessageBoxResult result = MessageBox.Show("PALAVRA 6 COMPLETA", "Parabéns:");
+                Palavra6 = true;
+            }
+
+            if (C8_1.Text.Contains("C") && C4_5.Text.Contains("A") && C8_3.Text.Contains("M") && C5_2.Text.Contains("I") && C8_5.Text.Contains("S") && C8_6.Text.Contains("E") && C8_7.Text.Contains("T") && C6_2.Text.Contains("A") && Palavra8 == false)
+            {
+                C8_1.Background = Brushes.LightGreen;
+                C4_5.Background = Brushes.LightGreen;
+                C8_3.Background = Brushes.LightGreen;
+                C5_2.Background = Brushes.LightGreen;
+                C8_5.Background = Brushes.LightGreen;
+                C8_6.Background = Brushes.LightGreen;
+                C8_7.Background = Brushes.LightGreen;
+                C6_2.Background = Brushes.LightGreen;
+                MessageBoxResult result = MessageBox.Show("PALAVRA 8 COMPLETA", "Parabéns:");
+                Palavra8 = true;
             }
         }
 
         private void VerificarP7(object sender, KeyEventArgs e)
         {
-            if (C3_2.Text.Contains("R") && C7_2.Text.Contains("O") && C7_3.Text.Contains("M") && C4_2.Text.Contains("A"))
+            if (C3_2.Text.Contains("R") && C7_2.Text.Contains("O") && C7_3.Text.Contains("M") && C4_2.Text.Contains("A") && Palavra7 == false)
             {
-                MessageBoxResult result = MessageBox.Show("PALAVRA 7 COMPLETA", "Dica:");
+                C3_2.Background = Brushes.LightGreen;
+                C7_2.Background = Brushes.LightGreen;
+                C7_3.Background = Brushes.LightGreen;
+                C4_2.Background = Brushes.LightGreen;
+                MessageBoxResult result = MessageBox.Show("PALAVRA 7 COMPLETA", "Parabéns:");
+                Palavra7 = true;
+            }
+        }
+
+        private void VerificarP8(object sender, KeyEventArgs e)
+        {
+            if (C8_1.Text.Contains("C") && C4_5.Text.Contains("A") && C8_3.Text.Contains("M") && C5_2.Text.Contains("I") && C8_5.Text.Contains("S") && C8_6.Text.Contains("E") && C8_7.Text.Contains("T") && C6_2.Text.Contains("A") && Palavra8 == false)
+            {
+                C8_1.Background = Brushes.LightGreen;
+                C4_5.Background = Brushes.LightGreen;
+                C8_3.Background = Brushes.LightGreen;
+                C5_2.Background = Brushes.LightGreen;
+                C8_5.Background = Brushes.LightGreen;
+                C8_6.Background = Brushes.LightGreen;
+                C8_7.Background = Brushes.LightGreen;
+                C6_2.Background = Brushes.LightGreen;
+                MessageBoxResult result = MessageBox.Show("PALAVRA 8 COMPLETA", "Parabéns:");
+                Palavra7 = true;
+            }
+        }
+
+        private void VerificarP9(object sender, KeyEventArgs e)
+        {
+            if (C4_1.Text.Contains("C") && C9_2.Text.Contains("U") && C9_3.Text.Contains("B") && C9_4.Text.Contains("O") && Palavra9 == false)
+            {
+                C4_1.Background = Brushes.LightGreen;
+                C9_2.Background = Brushes.LightGreen;
+                C9_3.Background = Brushes.LightGreen;
+                C9_4.Background = Brushes.LightGreen;
+                MessageBoxResult result = MessageBox.Show("PALAVRA 9 COMPLETA", "Parabéns:");
+                Palavra9 = true;
+            }
+        }
+
+        private void VerificarP10(object sender, KeyEventArgs e)
+        {
+            if (C10_1.Text.Contains("F") && C5_8.Text.Contains("O") && C10_3.Text.Contains("N") && C10_4.Text.Contains("E") && Palavra10 == false)
+            {
+                C10_1.Background = Brushes.LightGreen;
+                C5_8.Background = Brushes.LightGreen;
+                C10_3.Background = Brushes.LightGreen;
+                C10_4.Background = Brushes.LightGreen;
+                MessageBoxResult result = MessageBox.Show("PALAVRA 9 COMPLETA", "Parabéns:");
+                Palavra10 = true;
+            }
+        }
+
+        //private void VerificarVitoria(object sender, MouseEventArgs e)
+       // {
+       //     if(Palavra1 == true && Palavra2 == true && Palavra3 == true && Palavra4 == true && Palavra5 == true && Palavra6 == true && Palavra7 == true && Palavra8 == true && Palavra9 == true && Palavra10 == true)
+       //     {
+       //         txtVitoria.Text = "Vitória";
+       //     }
+      //  }
+
+        private void VerificarVitoria(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            if (Palavra1 == true)
+            {
+                MessageBoxResult result = MessageBox.Show("FOI");
+                txtVitoria.Text += "Vitória";
             }
         }
     }
